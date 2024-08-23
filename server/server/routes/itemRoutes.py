9ddf117b4e1ba58e41a56de257e5da0cd74f9f6f
Aser <ajhubero16@gmail.com>
2024-08-23 20:48:54 +0800

    Server - Added the route for deleting an item

--- server/server/routes/itemRoutes.py
+++ server/server/routes/itemRoutes.py
@@ -32,36 +32,39 @@ def isAuth(f):
 @app.route('/api/items', methods=['POST'])
 @isAuth
 def addItems():
+    try:
+
+        form = CreateItemForm()
     
-    form = CreateItemForm()
-  
-    if not form.validate_on_submit():  
-        first_error_message = next(iter(form.errors.values()))[0]
-        resBody = {"msg":first_error_message, "status":400}
-        return jsonify(resBody), 400
+        if not form.validate_on_submit():  
+            first_error_message = next(iter(form.errors.values()))[0]
+            resBody = {"msg":first_error_message, "status":400}
+            return jsonify(resBody), 400
+        
+        img_file = request.files['img']
+        if not img_file:
+            resBody = {"msg":"Product Image is a required field", "status":400}
+            return jsonify(resBody), 400
+
     
-    img_file = request.files['img']
-    if not img_file:
-        resBody = {"msg":"Product Image is a required field", "status":400}
-        return jsonify(resBody), 400
+        img_upload = cloudinary.uploader.upload(img_file)
+        
+        newProduct = Item(
+            title=form.title.data,
+            desc=form.desc.data,
+            price=form.price.data,
+            img=img_upload['url']
 
-  
-    img_upload = cloudinary.uploader.upload(img_file)
+        )
+
+        db.session.add(newProduct)
+        db.session.commit()
+
     
-    newProduct = Item(
-        title=form.title.data,
-        desc=form.desc.data,
-        price=form.price.data,
-        img=img_upload['url']
-
-    )
-
-    db.session.add(newProduct)
-    db.session.commit()
-
-   
-    resBody = {"msg": f'Created the Product {newProduct.title}!', "status":201 }
-    return jsonify(resBody), 201
+        resBody = {"msg": f'Created the Product {newProduct.title}!', "status":201 }
+        return jsonify(resBody), 201
+    except Exception as e:
+        print(e)
    
 
 """
@@ -73,33 +76,36 @@ def addItems():
 @app.route('/api/items/<int:id>', methods=['PUT'])
 @isAuth
 def editItem(id):
-    foundItem = Item.query.get(id)
-    if not foundItem:
-        return jsonify({"msg": "Item did not found", "status": 404}), 404
+    try:
 
-    form = EditItemForm()
+        foundItem = Item.query.get(id)
+        if not foundItem:
+            return jsonify({"msg": "Item did not found", "status": 404}), 404
 
-    if not form.validate_on_submit():
-        first_error_message = next(iter(form.errors.values()))[0]
-        return jsonify({"msg": first_error_message, "status": 400}), 400
-    
-    img_file = request.files['img']
-    if not img_file:
-        resBody = {"msg":"Product Image is a required field", "status":400}
-        return jsonify(resBody), 400
-    
-    img_upload = cloudinary.uploader.upload(img_file)
+        form = EditItemForm()
 
-    foundItem.title = form.title.data
-    foundItem.desc = form.desc.data
-    foundItem.price = form.price.data
-    foundItem.img = img_upload['url']
-    
-    db.session.commit()
+        if not form.validate_on_submit():
+            first_error_message = next(iter(form.errors.values()))[0]
+            return jsonify({"msg": first_error_message, "status": 400}), 400
+        
+        img_file = request.files['img']
+        if not img_file:
+            resBody = {"msg":"Product Image is a required field", "status":400}
+            return jsonify(resBody), 400
+        
+        img_upload = cloudinary.uploader.upload(img_file)
 
-    resBody = {"msg": f'Updatedthe Product {foundItem.title}!', "status":201 }
-    return jsonify(resBody), 201
+        foundItem.title = form.title.data
+        foundItem.desc = form.desc.data
+        foundItem.price = form.price.data
+        foundItem.img = img_upload['url']
+        
+        db.session.commit()
 
+        resBody = {"msg": f'Updated the Product {foundItem.title}!', "status":201 }
+        return jsonify(resBody), 201
+    except Exception as e:
+        print(e)
 """
 
 // params id = This is the ID of the product that we're going to use for fetching the information of the specific item
@@ -107,11 +113,34 @@ def editItem(id):
 """
 @app.route('/api/items/<int:id>')
 def getItemDetails(id):
-
-    foundItem = Item.query.get(id)
-    if not foundItem:
-        return jsonify({"msg": "Item did not found", "status": 404}), 404
+    try:
+        foundItem = Item.query.get(id)
+        if not foundItem:
+            return jsonify({"msg": "Item did not found", "status": 404}), 404
+        
+        serializedItem = foundItem.toDict()
+        return jsonify({"data": serializedItem, "status":201}), 201
     
-    serializedItem = foundItem.toDict()
+    except Exception as e:
+        print(e)
+
+"""
+
+// params id = This is the ID of the product that we're going to use for deleting the specific item
+
+"""
+
+@app.route('/api/items/<int:id>', methods=['DELETE'])
+def deleteItem(id):
+    try:
+        item = Item.query.get(id)
+        if not item:
+            return jsonify({"msg": "Item not found", "status": 404}), 404
+
    
-    return jsonify({"data": serializedItem, "status":201}), 201
+        db.session.delete(item)
+        db.session.commit()
+
+        return jsonify({"msg": "Item successfully deleted", "status": 200}), 200
+    except Exception as e:
+      print(e)
