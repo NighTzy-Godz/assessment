f1d2073085fda7a78c3c389080d77e81849297de
aser <ajhubero16@gmail.com>
2025-02-05 08:56:24 +0800

    Fix pnpm install by setting SHELL variable

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,4 +1,4 @@
+export SHELL=/bin/bash
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 export PATH="$HOME/.local/share/pnpm:$PATH"
-pnpm install --frozen-lockfile
-pnpm add -g ts-node
+pnpm install --frozen-lockfile --prod=false
