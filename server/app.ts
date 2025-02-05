1c9372c6b0d9d6ef44c82ddb933788a2e58c024c
aser <ajhubero16@gmail.com>
2025-02-05 09:32:34 +0800

    Added the CORS

--- server/app.ts
+++ server/app.ts
@@ -5,7 +5,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use("/api/user", userRoute);
 app.get("/healthcheck", (req, res) => {
