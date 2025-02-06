186fcdbeaa321182ee9742fd1483f40f228263e3
aser <ajhubero16@gmail.com>
2025-02-07 01:09:08 +0800

    Added the runWarmup to prevent the cold start of backend

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,9 @@ require("dotenv").config();
 import app from "./app";
 import { PORT } from "./config/env";
 import connectToDB from "./infrastructure/connectToDB";
+import { runWarmUp } from "./infrastructure/keepAlive";
 
 connectToDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  runWarmUp();
 });
